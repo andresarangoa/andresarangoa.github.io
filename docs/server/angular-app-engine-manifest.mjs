d0e62e02ab73bece71382ba8d8a9c99397da85aa
Andrés Felipe Arango Rodriguez <andresarangopro@gmail.com>
2025-02-04 11:30:58 -0500

    add i18n to assets

--- docs/server/angular-app-engine-manifest.mjs
+++ docs/server/angular-app-engine-manifest.mjs
@@ -1,6 +1,6 @@
 
 export default {
-  basePath: '/',
+  basePath: 'https://andresarangoa.github.io/browser',
   entryPoints: new Map([['', () => import('./main.server.mjs')]]),
 };
   
--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -5,33 +5,33 @@ export default {
   routes: [
   {
     "renderMode": 2,
-    "redirectTo": "/home",
-    "route": "/"
+    "redirectTo": "/https://andresarangoa.github.io/browser/home",
+    "route": "/https://andresarangoa.github.io/browser"
   },
   {
     "renderMode": 2,
-    "route": "/home"
+    "route": "/https://andresarangoa.github.io/browser/home"
   },
   {
     "renderMode": 2,
-    "route": "/projects"
+    "route": "/https://andresarangoa.github.io/browser/projects"
   },
   {
     "renderMode": 2,
-    "route": "/blog"
+    "route": "/https://andresarangoa.github.io/browser/blog"
   },
   {
     "renderMode": 2,
-    "route": "/contact"
+    "route": "/https://andresarangoa.github.io/browser/contact"
   }
 ],
   assets: new Map([
-['index.csr.html', {size: 2330, hash: '2400e6d3f7c9b8f5c0d7153f7c87709ae5e43a790a027cf2e0202233f205e27e', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)}], 
-['index.server.html', {size: 1144, hash: '06447dac335aff0b889a07708e07e70b058be771c56d2838c100e4bf3ebaaf08', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)}], 
-['projects/index.html', {size: 47904, hash: 'd2422cb6c07577f0ff58952cf6bd0c3c19590ad187df4c27bb01f7dd034ab3c7', text: () => import('./assets-chunks/projects_index_html.mjs').then(m => m.default)}], 
-['blog/index.html', {size: 21208, hash: 'b9d3c1d947dd935d8dac469e3a6a8f7996fd977b1f7d0fb2fc65644f427972fc', text: () => import('./assets-chunks/blog_index_html.mjs').then(m => m.default)}], 
-['home/index.html', {size: 47904, hash: 'd2422cb6c07577f0ff58952cf6bd0c3c19590ad187df4c27bb01f7dd034ab3c7', text: () => import('./assets-chunks/home_index_html.mjs').then(m => m.default)}], 
-['contact/index.html', {size: 21125, hash: '245c402a7a44b642c13bedfffdf76ffe267ec71377fcee6b2bfc6ab551c56cad', text: () => import('./assets-chunks/contact_index_html.mjs').then(m => m.default)}], 
+['index.csr.html', {size: 2368, hash: 'ad701e36b3ea727acc9799873a32ee75e6f325b2bac6c71e0e579a2388e72536', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)}], 
+['index.server.html', {size: 1182, hash: 'facc72c67271dc5590ba579b2c735e25e397c43ade8f95d7e64fadd22fa5017f', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)}], 
+['r/projects/index.html', {size: 20745, hash: '4a8f8fc41b4db3cdf9a6abf4aa709d8ec8b4364c1366787312cb9f861ac03051', text: () => import('./assets-chunks/r_projects_index_html.mjs').then(m => m.default)}], 
+['r/blog/index.html', {size: 20745, hash: '4a8f8fc41b4db3cdf9a6abf4aa709d8ec8b4364c1366787312cb9f861ac03051', text: () => import('./assets-chunks/r_blog_index_html.mjs').then(m => m.default)}], 
+['r/home/index.html', {size: 20745, hash: '4a8f8fc41b4db3cdf9a6abf4aa709d8ec8b4364c1366787312cb9f861ac03051', text: () => import('./assets-chunks/r_home_index_html.mjs').then(m => m.default)}], 
+['r/contact/index.html', {size: 20745, hash: '4a8f8fc41b4db3cdf9a6abf4aa709d8ec8b4364c1366787312cb9f861ac03051', text: () => import('./assets-chunks/r_contact_index_html.mjs').then(m => m.default)}], 
 ['styles-P2U6XG7Z.css', {size: 4970, hash: 'I2bPc10v8Bo', text: () => import('./assets-chunks/styles-P2U6XG7Z_css.mjs').then(m => m.default)}]
 ]),
   locale: undefined,
